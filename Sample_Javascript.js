a3996207847dd38775919d6fd7e4d58c51efc24a
Jaiasree2024 <161117319+Jaiasree2024@users.noreply.github.com>
2024-02-25 12:48:32 +0530

    Add files via upload

---
+++ Sample_Javascript.js
@@ -0,0 +1,13 @@
+let num;
+function checkOddOrEven(num){
+    if(num%2==0){
+        console.log(num +" is even number");
+    }
+    else{
+        console.log(num +" is odd number")
+    }
+
+}
+
+let number=5;
+checkOddOrEven(5);
