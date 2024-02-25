fbe60c347914a2a4446a7755a340dad51266d8eb
Jaiasree2024 <161117319+Jaiasree2024@users.noreply.github.com>
2024-02-25 21:23:30 +0530

    Add files via upload

---
+++ LoopingStatement_Javascript.js
@@ -0,0 +1,22 @@
+/*
+int val = 5;
+funtion to sum all the values between 1 to val
+and return the sum
+// activity ---> print between 1, 10 --> keep adding the sum of values
+// 1, 2 (3), 3 (6), 4 (10), 5 (15)
+// finally, print the sum !! 
+
+*/
+
+var num =5;
+function addition(sum)
+{
+    var total = 0; 
+    for (i=0;i<=num;i++)
+    {
+        console.log(i);
+        total += i;
+    }
+    console.log(`Total amount is ${total}`);
+}
+addition(num)
